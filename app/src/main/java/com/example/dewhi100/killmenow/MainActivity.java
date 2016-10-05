5d3a2ef246cf4ab9223edc5a7e189d64ad3de5c6
dewhi100 <mrseandewhirst@gmail.com>
2016-10-04 20:07:50 -0400

    Followed a tutorial for making a simple app.  2 warnings... might look into them later.

--- app/src/main/java/com/example/dewhi100/killmenow/MainActivity.java
+++ app/src/main/java/com/example/dewhi100/killmenow/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dewhi100.killmenow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,19 +13,19 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    public final static String EXTRA_MESSAGE = "com.example.dewhi100.KMN.MainActivity.extraMessage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
